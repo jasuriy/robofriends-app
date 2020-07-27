d4aa52c65a45fc499baf10d4102e9bb13866f128
jasuriy <jasur1998gopirjonov@gmail.com>
2020-07-27 18:30:41 +0500

    Updates

---
+++ precache-manifest.0737fe3d6cb18cff488e2ef921029f03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac694f409d26162aa964652f7596121d",
+    "url": "/robofriends-app/index.html"
+  },
+  {
+    "revision": "87ee6da0e2e2baf471d2",
+    "url": "/robofriends-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fcaf2a6547009a4bcf6f",
+    "url": "/robofriends-app/static/css/main.51867315.chunk.css"
+  },
+  {
+    "revision": "87ee6da0e2e2baf471d2",
+    "url": "/robofriends-app/static/js/2.0d040641.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends-app/static/js/2.0d040641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcaf2a6547009a4bcf6f",
+    "url": "/robofriends-app/static/js/main.b35a09f5.chunk.js"
+  },
+  {
+    "revision": "ae97c924fe7288b454c8",
+    "url": "/robofriends-app/static/js/runtime-main.ced9dc27.js"
+  }
+]);
